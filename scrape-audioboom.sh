cb22fffa3837228ac79926c9af8cb9aed5a6610f
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2021-03-11 07:38:28 -0800

    Actualize the script

--- scrape-audioboom.sh
+++ scrape-audioboom.sh
@@ -5,7 +5,7 @@ set -e
 # recursively snag podcast episodes off audioboom.com
 getmp3 () {
   echo "Snagging $1"
-  echo curl -Lso $(echo $1 | sed -e "s/.*[0-9]-/$2-/").mp3 "$1".mp3
+  curl -Lso $(echo $1 | sed -e "s/.*[0-9]-/$2-/").mp3 "$1".mp3
   next=$(curl -s "$1" | grep playlist_direction | grep -v cdn | sed -e 's/.*href="//' -e 's/\?playlist_direction.*//')
   getmp3 "https:"$next $(($2 + 1))
 }
